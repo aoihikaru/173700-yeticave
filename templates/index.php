14a1b9e9ebea3f107b52519993ae69ad827c322c
aoihikaru <aoi.hikaru13@gmail.com>
2019-02-21 21:34:50 +0300

    Шаблонизация проекта

---
+++ templates/index.php
@@ -0,0 +1,34 @@
+<section class="lots">
+	<div class="lots__header">
+		<h2>Открытые лоты</h2>
+	</div>
+	<ul class="lots__list">
+
+		<?php foreach ($auction as $lot): ?>
+
+			<!--заполните этот список из массива с товарами-->
+			<li class="lots__item lot">
+				<div class="lot__image">
+					<img src="<?=check_text($lot['pic_url']) ?>" width="350" height="260" alt="<?=check_text($lot['name']) ?>">
+				</div>
+				<div class="lot__info">
+					<span class="lot__category"><?=$lot['category'] ?></span>
+					<h3 class="lot__title"><a class="text-link" href="../pages/lot.html"><?=check_text($lot['name']) ?></a></h3>
+					<div class="lot__state">
+						<div class="lot__rate">
+							<span class="lot__amount">Стартовая цена</span>
+							<span class="lot__cost">
+                                        <?=(price_change($lot['price']));?>
+                            </span>
+						</div>
+						<div class="lot__timer timer">
+							12:23
+						</div>
+					</div>
+				</div>
+			</li>
+
+		<?php endforeach; ?>
+
+	</ul>
+</section>
